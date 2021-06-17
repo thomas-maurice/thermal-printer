bcd060457ad10da1322b9af61a5e209639567a28
Thomas Maurice <thomas@maurice.fr>
2021-06-17 14:58:30 +0100

    Build

--- setup.py
+++ setup.py
@@ -7,20 +7,31 @@ with open("requirements.txt", "r") as fh:
     deps = fh.read().split("\n")
 
 setuptools.setup(
-    name="thermal-printer-controller", # Replace with your own username
+    name="thermal-printer-server",
     version="0.0.1",
     author="Thomas Maurice",
     author_email="thomas@maurice.fr",
-    description="Thermal printer controller",
+    description="Thermal printer server",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/thomas-maurice/thermal-printer",
     packages=setuptools.find_packages(),
+    keywords=[
+        "escpos",
+        "printer",
+    ],
+    scripts=[
+        "server/escpos-server",
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.4',
     install_requires=deps,
 )
